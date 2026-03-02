35122521c4da58dade3ef1e1d896fcbfcc8805ba
Suretrader <suretrader141@gmail.com>
2026-03-02 05:23:46 -0800

    hospital

--- src/conponents/FaqFrequently.jsx
+++ src/conponents/FaqFrequently.jsx
@@ -1,41 +1,145 @@
 import React from 'react'
 import FaqImg from "../assets/FaqSmiling.jpg"
 import { Link } from 'react-router-dom'
+import { Accordion, AccordionContent, AccordionPanel, AccordionTitle } from "flowbite-react";
 
 
 const FaqFrequently = () => {
   return (
-    <div  className='m-20 grid md:grid-cols-2 gap-20'>
-      <section>
+    <div  className='m-20 grid grid-cols-3 gap-20'>
+      <section className='col-span-2'>
        <div>
          <h1 className='text-3xl md:text-4xl font-bold mt-20 mb-10 '>Frequently Asked Questions</h1>
-         <p>Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor tempus feugiat dolor lacinia cubilia curae integer congue leo metus, primis in orci integer metus mollis faucibus enim. Nemo ipsam egestas volute turpis dolores ut aliquam quaerat sodales sapien undo pretium purus feugiat dolor impedit magna purus pretium gravida donec ligula massa gravida donec pretium</p>
+            <h2 className='font-bold text-2xl'>Do you speak my language?</h2>
+       <p>We offer Free Interpretation Services for over 200 languages via video and phone. Simply point to your language on our Language Identification Poster at any reception desk.</p>
        </div>
        <div>
          <h1  className='font-bold text-blue-400 text-2xl mt-10 mb-10'>1. General Questions</h1>
-         <h3 className="mb-6 text-xl font-bold">Is porta semper MedService primis?   </h3>
-         <p className='mb-3'>Nullam rutrum eget nunc varius etiam mollis risus congue aliquam etiam sapien egestas, congue gestas posuere cubilia congue ipsum mauris lectus laoreet gestas neque vitae auctor eros dolor luctus odio tempor sapien gravida donec pretium and ipsum porta</p>
-         <h4 className="mb-3 text-xl font-bold border-t">How feugiat primis ultrice in ligula?</h4>
-         <h4 className="mb-3 text-xl font-bold border-t">Did donec pretium and MedService porta?</h4>
-         <h4 className="mb-3 text-xl font-bold border-t">What is metus mollis faucibus?</h4>
+        <Accordion>
+           <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>What should I bring?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+         Bring photo ID, insurance card, medication list, and relevant records.          </p>
+          
+        </AccordionContent>
+      </AccordionPanel>
+      <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>Medical records?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+          Complete a release form via our Medical Records Department (allow 7-10 days).          </p>
+        
+        </AccordionContent>
+      </AccordionPanel>
+      <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>What are the hospital’s operating hours?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+         While most hospitals are open 24/7 for emergencies, specify the hours for regular outpatient clinics and administrative offices (e.g., Mon–Fri, 8:00 am – 5:00 pm).          </p>
+        </AccordionContent>
+      </AccordionPanel>
+      <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>Where is the hospital located?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+         10 Adepitan st, ojodu, lagos 101232,lagos
+
+          </p>
+        
+        </AccordionContent>
+      </AccordionPanel>
+    </Accordion>
        </div>
        <div>
          <h1 className='font-bold text-blue-400 text-2xl mt-10 mb-10'>2. Medical Treatment Questions</h1>
-         <h3 className="mb-3 text-xl font-bold border-b py-3">How much congue magna nihil impedit ligula risus?</h3>
-         <h3 className="mb-3 text-xl font-bold border-b py-3">What I congue magna nihil convallis porta?</h3>
-         <h3 className="mb-3 text-xl font-bold border-b py-3">Is there a aliquet elementum massa?</h3>
-         <h3 className="mb-3 text-xl font-bold border-b py-3">Can I auctor eros dolor luctus odio placerat massa magna?</h3>
-         <h3 className="mb-3 text-xl font-bold border-b py-3">Do you have vehicula magna luctus tempor?</h3>
-       </div>
+         <Accordion>
+           <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>Why do I need this specific test?
+          </AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+          Explain that tests like blood work, X-rays, or CT scans help the doctor confirm a diagnosis, rule out other conditions, or monitor how well a current treatment is working.          </p>
+        </AccordionContent>
+      </AccordionPanel>
+      <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>What should I do to prepare for my test?
+</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+          Common instructions include fasting (no food or drink) for 8–12 hours, stopping certain medications, or arriving with a full bladder.          </p>
+        </AccordionContent>
+      </AccordionPanel>
+      <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>How and when will I get my results?
+       </AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+          Most results are available within 24–48 hours, though specialized labs may take longer. Patients can often access them via an online patient portal or during a follow-up call.          </p>
+        </AccordionContent>
+      </AccordionPanel>
+      <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>What does an "abnormal" result mean?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            An abnormal result doesn't always mean something is wrong; it may require a repeat test or further investigation to understand its significance in the context of your overall health.
+          </p>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+           sometimes its wrong
+          </p>
+          
+          
+        </AccordionContent>
+      </AccordionPanel>
+      <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>What are all these tubes and machines for?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+           Encourage patients to ask their nurse to explain the purpose of IV lines, monitors, and oxygen equipment to reduce anxiety.          </p>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+           READ THE FIRST PARAGRAPH AGAIN
+          </p>
+          
+          
+        </AccordionContent>
+      </AccordionPanel>
+    </Accordion>
+     </div>
        <div>
          <h1 className='font-bold text-blue-400 text-2xl mt-10 mb-10' >3. Emergency Help Questions</h1>
-         <h3 className="mb-3 text-xl font-bold border-b py-3">I have tempor sapien gravida?</h3>
-         <h3 className="mb-3 text-xl font-bold border-b py-3">I want molestie bibendum quis, aliquet?</h3>
-         <h3 className="mb-3 text-xl font-bold border-b py-3">Can I tincidunt tincidunt massa, et porttitor?</h3>
+        <Accordion>
+           <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>What tests are being ordered and why?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+           Understand the purpose of every X-ray, blood draw, or scan to stay informed about your care plan.        </p>
+        </AccordionContent>
+      </AccordionPanel>
+           <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>What is the most likely cause of my symptoms?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+           Ask for both the primary diagnosis and any "differential" (alternative) possibilities the doctor is ruling out.
+          </p>
+        </AccordionContent>
+      </AccordionPanel>
+      <AccordionPanel>
+        <AccordionTitle className='font-bold text-1xl'>What are the 'red flags' I should watch for after I leave?</AccordionTitle>
+        <AccordionContent>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+          Before discharge, ask which specific symptoms mean you must return to the ER immediately. 
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+          WATCH YOUR WEIGHT TOO
+          </p>
+        </AccordionContent>
+      </AccordionPanel>
+      
+    </Accordion>
        </div>
      </section>
          
-     <section>
+     <section className='col-span-1'>
           <h3  className='font-bold text-2xl mb-4  border-b py-5'>The Heart Of Clinic</h3>
         <div className=' flex flex-row py-2 gap-5'>
           <div>
@@ -44,17 +148,16 @@ const FaqFrequently = () => {
           <div className='mt-10'>
             <h4 className='font-semibold text-2xl mb-2'> Dr. jonathan Barnes</h4>
             <p>Chief Medical Officer, Founder</p>
-            <h2 className='font-bold text-blue-400 text-3xl'>1-800-1234-567</h2>
+            <h2 className='font-bold text-blue-400 text-2xl'>1-800-1234-567</h2>
           </div>
         </div>
-        <p className='py-2'>An enim nullam tempor sapien at gravida donec <br /> pretium ipsum porta justo integer at odiovelna vitae <br /> auctor integer congue magna purus</p>
+        <p className='py-2'>Dr. Jenkins is a board-certified pediatrician with over 12 years of experience in newborn care and adolescent wellness. He believes in a family-centered approach to medicine, focusing on preventive health and open communication with parents to ensure every child reaches their full potential.</p>
         <button className='bg-blue-400 rounded p-3 text-white'>Learn More</button>
          <div className="bg-blue-400 p-10 text-white rounded-lg mt-10 w-90">
                    <div>
                         <h3 className='text-2xl font-bold mb-6'>Working Time</h3>
     <p className='mb-8 text-white/90'>
-       Porta semper lacus cursus, feugiat primis ultrice ligula risus auctor at tempus feugiat dolor lacinia cursus nulla vitae massa
-    </p>
+At Our Hospital, we provide care around the clock. While our Emergency Department is open 24/7, 365 days a year.    </p>
                    </div>
        <div className='space-y-6'>
        <div className='flex justify-between border-b-2 border-dotted border-white/30 pb-4'>
@@ -86,7 +189,7 @@ const FaqFrequently = () => {
     
             </div>
             <h3 className='text-1xl font-bold mb-6'>Need a personal health plan?</h3>
-            <p className='mt-2'>Porta semper lacus cursus, and feugiat primis ultrice ligula at risus auctor</p>
+            <p className='mt-2'>Rotate your health plans with weather updates, clinic news, or relaxing nature loops to prevent "screen fatigue</p>
        </div>
 
 
@@ -96,7 +199,7 @@ const FaqFrequently = () => {
 
                   <div className='border rounded mt-10 p-8 w-85'>
           <h2  className='font-bold text-2xl mb-4 py-5' >Doctors Timetable</h2>
-          <p>Porta semper lacus cursus, feugiat primis <br /> ultrice ligula risus auctor at tempus feugiat <br /> dolor lacinia cursus nulla vitae massa</p>
+          <p>Our specialists are available for consultations Monday through Friday from 8:00 AM to 5:00 PM, with extended evening clinics on Tuesdays and Thursdays. Saturday sessions are reserved for pre-booked follow-ups and wellness screenings between 9:00 AM and 1:00 PM. For immediate medical needs outside these hours</p>
            <button className='bg-blue-400 rounded p-3 mt-5 text-white'>View Timetable</button>
          </div>
      </section>
